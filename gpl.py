f61b3ac40683522fedbb684e6381808779e622ca
Georg Prohaska <georg.prohaska@gmail.com>
2017-11-12 08:35:13 +0100

    started stage 2, directed graph working, starting on neighbour repr

--- gpl.py
+++ gpl.py
@@ -11,15 +11,31 @@
 
 import unittest
 
-class Base:
+class Base():
   #
   # Begin of my implementation
   # ---------------%<------------------
-  #
   
   class Node:
-      pass
+      def __init__(self):
+          self._neighbours = []
           
+      def neighbours(self):
+          return self._neighbours
+          
+      def addNeighbour(self, neighbour):
+          self._neighbours.append(neighbour)
+          
+      def addNeighbours(self, neighbours):
+          self._neighbours.extend(neighbours)
+          
+  class Neighbour:
+      def __init__(self, opposite):
+          self._opposite = opposite
+          
+      def opposite(self):
+          return self._opposite
+      
   class Edge:
       def __init__(self, node1, node2):
           self._node1 = node1
@@ -30,25 +46,43 @@ class Base:
       def node2(self):
           return self._node2
 
-  class Graph:
-      def __init__(self):
+  class Graph():
+        # "Constants"
+      NEIGHBOUR = "neighbour"
+      EDGE = "edge"
+      
+      def __init__(self, directed = False, representation = None):
+          if representation == None:
+              representation = Base.Graph.EDGE
+          self._directed = directed
+          self._representation = representation
           self._nodes = {}
           self.nodeCoutner = 0
-          self._edges = []
+          if self._representation == Base.Graph.EDGE:  
+                self._edges = []
       
       def nodeID(self, node):
           return self._nodes.get(node)
       
       def edges(self):
-          return self._edges
+          if self._representation == Base.Graph.EDGE:
+              return self._edges
+          else:
+              return None
       
+      #add an edge
+      #in digraphs the direction is from newNode1 to newNode2
       def add(self, newNode1, newNode2):
+          #use different method for neighbour representation
+          if self._representation == Base.Graph.NEIGHBOUR:
+                    return self._add(newNode1, newNode2)
           # check if edge already exists
           for edge in self._edges:
               if edge.node1() is newNode1 and edge.node2() is newNode2:
                   return edge
-              elif edge.node1() is newNode2 and edge.node2() is newNode1:
-                  return edge
+              if not self._directed:
+                  if edge.node1() is newNode2 and edge.node2() is newNode1:
+                      return edge
               
           #add the new edge
           e = Base.Edge(newNode1, newNode2)
@@ -62,23 +96,49 @@ class Base:
               self.nodeCoutner += 1
           return e
       
+      #"private" method for adding edges in neighbour representation
+      def _add(self, newNode1, newNode2):
+          #check if edge already exists
+          for node in self._nodes.keys():
+              if node is newNode1:
+                  for neighbour in node.neighbours():
+                      if neighbour.opposite() is newNode2:
+                          return neighbour
+          
+          if newNode1 in self._nodes.keys():
+              neighbour = Base.Neighbour(newNode2)
+              newNode1.addNeighbour(neighbour)
+              
+              
+              
+              
+          return
+      
       def size(self):
           return len(self._nodes)
       
       def numEdges(self):
           return len(self._edges)
   
-  #DOT pretty printer
-  def DOTprint(self, graph):
-      #start a graph
-      DOTstring = 'graph g{\nnode[label=""]; \n'
-      #add every edge
-      for edge in graph.edges():
-          DOTstring += str(graph.nodeID(edge.node1())) + " -- " + str(graph.nodeID(edge.node2())) + "\n"
-      #finish graph
-      DOTstring += "}"
-      return DOTstring
-      
+      #DOT pretty printer
+      def DOTprint(self):
+          #start a graph
+          if self._directed:
+              DOTstring = 'digraph g{\nnode[label=""]; \n'
+          else:
+              DOTstring = 'graph g{\nnode[label=""]; \n'
+          #add every edge
+          for edge in self._edges:
+              if self._directed:
+                  DOTstring += (str(self._nodes.get(edge.node1())) + " -> " + 
+                                str(self._nodes.get(edge.node2())) + "\n")
+              else:
+                  DOTstring += (str(self._nodes.get(edge.node1())) + " -- " + 
+                                str(self._nodes.get(edge.node2())) + "\n")
+          #finish graph
+          DOTstring += "}"
+          return DOTstring
+          
   # 
   # ---------------%<------------------
   # End of my implementation
@@ -120,8 +180,6 @@ class Test(unittest.TestCase):
       G.add(n4, n1)
       self.assertEqual(G.size(), 4)
       
-      #test pretty printer
-      B = Base()
       #create graph from assignment specification
       G = Base.Graph()
       n5, n6, n7, n8 = Base.Node(), Base.Node(), Base.Node(), Base.Node()
@@ -133,8 +191,8 @@ class Test(unittest.TestCase):
       G.add(n5, n7)
       G.add(n5, n8)
       
-      print(B.DOTprint(G))
-      self.assertEqual(B.DOTprint(G), "graph g{\nnode[label=\"\"]; \n" \
+      print(G.DOTprint())
+      self.assertEqual(G.DOTprint(), "graph g{\nnode[label=\"\"]; \n" \
                        "0 -- 1\n" \
                        "0 -- 2\n" \
                        "0 -- 3\n" \
@@ -143,6 +201,26 @@ class Test(unittest.TestCase):
                        "4 -- 6\n" \
                        "4 -- 7\n" \
                        "}")
+                  
+      #test directed graphs 
+      G = Base.Graph(directed = True)
+      G.add(n1, n2)
+      G.add(n2, n3)
+      G.add(n2, n4)
+      G.add(n2, n5)
+      G.add(n6, n5)
+      G.add(n5, n7)
+      G.add(n5, n8)
+      print(G.DOTprint())
+      self.assertEqual(G.DOTprint(), "digraph g{\nnode[label=\"\"]; \n" \
+               "0 -> 1\n" \
+               "1 -> 2\n" \
+               "1 -> 3\n" \
+               "1 -> 4\n" \
+               "5 -> 4\n" \
+               "4 -> 6\n" \
+               "4 -> 7\n" \
+               "}")
       # ---------------%<------------------
       # End of my tests
       #    
